d033d5bd5c95341bcb6872bcb2a9a132c4473859
confucianzuoyuan <zuoyuantc@126.com>
2022-08-23 19:47:46 +0800

    update

---
+++ chapter12/src/asm.rs
@@ -0,0 +1,55 @@
+use frame::Frame;
+use temp::{Label, Temp};
+
+#[derive(Debug)]
+pub enum Instruction {
+    Operation {
+        assembly: String,
+        destination: Vec<Temp>,
+        source: Vec<Temp>,
+        jump: Option<Vec<Label>>,
+    },
+    Label {
+        assembly: String,
+        label: Label,
+    },
+    Move {
+        assembly: String,
+        destination: Vec<Temp>,
+        source: Vec<Temp>,
+    },
+}
+
+impl Instruction {
+    pub fn to_string<F: Frame>(&self) -> String {
+        match *self {
+            Instruction::Label { ref assembly, .. } => assembly.clone(),
+            Instruction::Move {
+                ref assembly,
+                ref destination,
+                ref source,
+            }
+            | Instruction::Operation {
+                ref assembly,
+                ref destination,
+                ref source,
+                ..
+            } => {
+                let mut result = assembly.clone();
+                for (index, temp) in destination.iter().enumerate() {
+                    result = result.replace(&format!("'d{}", index), &temp.to_string::<F>());
+                }
+                for (index, temp) in source.iter().enumerate() {
+                    result = result.replace(&format!("'s{}", index), &temp.to_string::<F>());
+                }
+                result
+            }
+        }
+    }
+}
+
+pub struct Subroutine {
+    pub prolog: String,
+    pub body: Vec<Instruction>,
+    pub epilog: String,
+}
---
+++ chapter12/src/position.rs
@@ -0,0 +1,87 @@
+use std::fmt::{self, Display, Formatter};
+use std::u32;
+
+use symbol::{Symbol, Symbols};
+use terminal::Terminal;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Pos {
+    pub byte: u64,
+    pub column: u32,
+    pub file: Symbol,
+    pub length: usize,
+    pub line: u32,
+}
+
+impl Pos {}
+
+impl Pos {
+    pub fn new(line: u32, column: u32, byte: u64, file: Symbol, length: usize) -> Self {
+        Pos {
+            byte,
+            column,
+            file,
+            length,
+            line,
+        }
+    }
+
+    pub fn dummy() -> Self {
+        Self::new(u32::MAX, u32::MAX, u64::MAX, 0, 0)
+    }
+
+    pub fn grow(&self, pos: Pos) -> Self {
+        Pos {
+            byte: self.byte,
+            column: self.column,
+            file: self.file,
+            length: (pos.byte - self.byte) as usize + pos.length,
+            line: self.line,
+        }
+    }
+
+    pub fn show(&self, symbols: &Symbols<()>, terminal: &Terminal) {
+        let filename = symbols.name(self.file);
+        eprintln!(
+            "   {}{}-->{}{} {}:{}:{}",
+            terminal.bold(),
+            terminal.blue(),
+            terminal.reset_color(),
+            terminal.end_bold(),
+            filename,
+            self.line,
+            self.column
+        )
+    }
+}
+
+impl Display for Pos {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        write!(formatter, "{}:{}:", self.line, self.column)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct WithPos<T> {
+    pub node: T,
+    pub pos: Pos,
+}
+
+impl<T> WithPos<T> {
+    pub fn new(node: T, pos: Pos) -> Self {
+        Self { node, pos }
+    }
+
+    pub fn dummy(node: T) -> Self {
+        Self {
+            node,
+            pos: Pos::dummy(),
+        }
+    }
+}
+
+impl<T: PartialEq> PartialEq for WithPos<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.node == other.node
+    }
+}
